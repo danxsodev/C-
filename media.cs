be61df21327433b824792a55285ccf1b5a61f144
danxsodev <102252843+danxsodev@users.noreply.github.com>
2022-04-06 16:18:14 -0300

    Add files via upload

---
+++ media.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float w;
+            float x;
+            float y;
+            float z;
+            float r;
+
+            Console.WriteLine("digita o valor de w");
+            w=float.Parse(Console.ReadLine());
+            Console.WriteLine("digita o valor de x");
+            x=float.Parse(Console.ReadLine());
+            Console.WriteLine("digita o valor de y");
+            y=float.Parse(Console.ReadLine());
+            Console.WriteLine("digita o valor de z");
+            z=float.Parse(Console.ReadLine());
+         
+            r=(w+x+y+z)/4;
+            if(r>=6)
+            {
+                Console.WriteLine($"Aluno Aprovado, com a média de {r}");
+            } 
+            else if (r<6)
+            {
+                Console.WriteLine($"Aluno Reprovado, com a média de {r}");
+            }
+        }
+    }
+}
+
